f2476d49cabdc7e85039882be361114d581804f1
Sandeepreddy55 <114462132+Sandeepreddy55@users.noreply.github.com>
2023-11-03 08:39:11 +0530

    Update water jug prgm python.py

--- water jug prgm python.py
+++ water jug prgm python.py
@@ -1,54 +1,71 @@
-import heapq
-goal_state = (1, 2, 3, 8, 0, 4, 7, 6, 5)
-initial_state = (2, 8, 3, 1, 6, 4, 7, 0, 5)
-def manhattan_distance(state):
-    distance = 0
-    for i in range(9):
-        if state[i] != 0:
-            goal_row = (state[i] - 1) // 3
-            goal_col = (state[i] - 1) % 3
-            curr_row = i // 3
-            curr_col = i % 3
-            distance += abs(goal_row - curr_row) + abs(goal_col - curr_col)
-    return distance
-def generate_moves(state):
-    moves = []
-    empty_index = state.index(0)
-    if empty_index - 3 >= 0:
-        moves.append(empty_index - 3)
-    if empty_index + 3 < 9:
-        moves.append(empty_index + 3)
-    if empty_index % 3 != 0:
-        moves.append(empty_index - 1)
-    if (empty_index + 1) % 3 != 0:
-        moves.append(empty_index + 1)
-    return moves
-def solve_puzzle(initial_state):
-    open_set = [(manhattan_distance(initial_state), initial_state)]
-    heapq.heapify(open_set)
-    closed_set = set()
-    parent_map = {initial_state: None}
-    while open_set:
-        _, current_state = heapq.heappop(open_set)
-        if current_state == goal_state:
-            path = []
-            while current_state is not None:
-                path.insert(0, current_state)
-                current_state = parent_map[current_state]
-            return path
-        closed_set.add(current_state)
-        moves = generate_moves(current_state)
-        for move in moves:
-            new_state = list(current_state)
-            new_state[current_state.index(0)], new_state[move] = new_state[move], new_state[current_state.index(0)]
-            new_state = tuple(new_state)
-            if new_state not in closed_set:
-                if (manhattan_distance(new_state), new_state) not in open_set:
-                    heapq.heappush(open_set, (manhattan_distance(new_state), new_state))
-                    parent_map[new_state] = current_state
-solution_path = solve_puzzle(initial_state)
-for step, state in enumerate(solution_path):
-    print(f"Step {step}:")
-    for i in range(0, 9, 3):
-        print(state[i:i+3])
-    print()
+from collections import deque
+def Solution(a, b, target):
+	m = {}
+	isSolvable = False
+	path = []
+
+	q = deque()
+
+	#Initializing with jugs being empty
+	q.append((0, 0))
+
+	while (len(q) > 0):
+
+		# Current state
+		u = q.popleft()
+		if ((u[0], u[1]) in m):
+			continue
+		if ((u[0] > a or u[1] > b or
+			u[0] < 0 or u[1] < 0)):
+			continue
+		path.append([u[0], u[1]])
+
+		m[(u[0], u[1])] = 1
+
+		if (u[0] == target or u[1] == target):
+			isSolvable = True
+
+			if (u[0] == target):
+				if (u[1] != 0):
+					path.append([u[0], 0])
+			else:
+				if (u[0] != 0):
+
+					path.append([0, u[1]])
+
+			sz = len(path)
+			for i in range(sz):
+				print("(", path[i][0], ",",
+					path[i][1], ")")
+			break
+
+		q.append([u[0], b]) # Fill Jug2
+		q.append([a, u[1]]) # Fill Jug1
+
+		for ap in range(max(a, b) + 1):
+			c = u[0] + ap
+			d = u[1] - ap
+
+			if (c == a or (d == 0 and d >= 0)):
+				q.append([c, d])
+
+			c = u[0] - ap
+			d = u[1] + ap
+
+			if ((c == 0 and c >= 0) or d == b):
+				q.append([c, d])
+
+		q.append([a, 0])
+
+		q.append([0, b])
+
+	if (not isSolvable):
+		print("Solution not possible")
+
+if __name__ == '__main__':
+
+	Jug1, Jug2, target = 4, 3, 2
+	print("Path from initial state "
+		"to solution state ::")
+
+	Solution(Jug1, Jug2, target)
